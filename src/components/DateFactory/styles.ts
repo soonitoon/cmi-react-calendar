a63ff5b31d75fd8d1b7508a8c7b475b9229e1858
Hyuno Choi <soonitoon@gmail.com>
2021-09-23 11:47:23 +0900

    Refactor: Convert JS to TS (#13)

---
+++ src/components/DateFactory/styles.ts
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+interface IDateLi {
+  today: string;
+  isgray: string;
+}
+
+const DateLi = styled.li<IDateLi>`
+  color: ${(props) => props.today} !important;
+  color: ${(props) => props.isgray} !important;
+`;
+
+export default DateLi;
---
+++ src/components/WeekBar/WeekBar.tsx
@@ -0,0 +1,18 @@
+import React, { ReactElement } from "react";
+import Week from "./styles";
+
+const WeekBar: React.FC = (): ReactElement => {
+  const days: string[] = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return (
+    <Week>
+      {days.map(
+        (day: string): React.ReactElement => (
+          <li key={day}>{day}</li>
+        )
+      )}
+    </Week>
+  );
+};
+
+export default WeekBar;
